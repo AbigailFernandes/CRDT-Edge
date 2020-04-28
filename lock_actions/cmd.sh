56e225755afb9ccf248919a7fd0b1865a5d17aca
Shreshtha Pankaj <shreshtha.pankaj@colorado.edu>
2020-04-28 15:25:32 -0600

    new todo link

--- lock_actions/cmd.sh
+++ lock_actions/cmd.sh
@@ -1,10 +1,10 @@
 ../start_redis.sh
 sudo npm install
 zip -r action.zip *
-wsk -i action delete todo
-wsk -i action create todo --kind nodejs:10 action.zip --web true
-wsk -i action get todo --url
-wsk -i api create /api/todo get todo --apiname "todo"
-wsk -i api create /api/todo post todo --apiname "todo"
-wsk -i api create /api/todo delete todo --apiname "todo"
-wsk -i api create /api/todo put todo --apiname "todo"
+wsk -i action delete todolock
+wsk -i action create todolock --kind nodejs:10 action.zip --web true
+wsk -i action get todolock --url
+wsk -i api create /api/todolock get todolock --apiname "todolock"
+wsk -i api create /api/todolock post todolock --apiname "todolock"
+wsk -i api create /api/todolock delete todolock --apiname "todolock"
+wsk -i api create /api/todolock put todolock --apiname "todolock"
